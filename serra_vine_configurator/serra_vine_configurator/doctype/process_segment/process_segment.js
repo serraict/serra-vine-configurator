19057e652296727d76036f05ad6ebf92cef821af
marijn <marijn@serraict.com>
2025-04-09 16:54:37 +0200

    Add process segment

---
+++ serra_vine_configurator/serra_vine_configurator/doctype/process_segment/process_segment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Serra ICT and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Process Segment", {
+// 	refresh(frm) {
+
+// 	},
+// });
