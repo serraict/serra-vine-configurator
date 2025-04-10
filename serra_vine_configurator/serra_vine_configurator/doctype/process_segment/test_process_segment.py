510e31dba6dac43423c7c4b791d21d8034afdf34
marijn <marijn@serraict.com>
2025-04-10 15:29:52 +0200

    Add controller test

--- serra_vine_configurator/serra_vine_configurator/doctype/process_segment/test_process_segment.py
+++ serra_vine_configurator/serra_vine_configurator/doctype/process_segment/test_process_segment.py
@@ -4,6 +4,7 @@
 import frappe
 from frappe.tests import IntegrationTestCase, UnitTestCase
 
+from .process_segment import ProcessSegment
 
 # On IntegrationTestCase, the doctype test records and all
 # link-field test record dependencies are recursively loaded
@@ -17,16 +18,30 @@ class UnitTestProcessSegment(UnitTestCase):
 	Unit tests for ProcessSegment.
 	Use this class for testing individual functions and methods.
 	"""
+
 	def test_equality(self):
 		"""Test equality of ProcessSegment instances."""
 		# Create two instances of ProcessSegment
 		process_segment1 = frappe.get_doc({"doctype": "Process Segment", "name": "Test Segment 1"})
 		process_segment2 = frappe.get_doc({"doctype": "Process Segment", "name": "Test Segment 2"})
+		process_segment1b = frappe.get_doc({"doctype": "Process Segment", "name": "Test Segment 1"})
 
 		# Check if they are equal
 		self.assertEqual(process_segment1, process_segment1)
 		self.assertEqual(process_segment2, process_segment2)
 		self.assertNotEqual(process_segment1, process_segment2)
+		self.assertNotEqual(process_segment1, process_segment1b)
+
+	def test_controller(self):
+		"""Test controller of ProcessSegment."""
+		# This is loaded from the database actually:
+		doc = frappe.get_doc({"doctype": "Process Segment", "name": "Oppotten"})
+		self.assertIsNotNone(doc)
+		ps = ProcessSegment(doc)
+		self.assertIsNotNone(ps)
+		print(ps.name)
+		print(ps.doctype)
+		print(ps)
 
 
 class IntegrationTestProcessSegment(IntegrationTestCase):
