bb491f480496fe3540d79d4570cf8080ca877482
marijn <marijn@serraict.com>
2025-04-23 14:43:55 +0200

    Add product segments

---
+++ serra_vine_configurator/serra_vine_configurator/doctype/product_segment/product_segment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Serra ICT and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProductSegment(Document):
+	pass
